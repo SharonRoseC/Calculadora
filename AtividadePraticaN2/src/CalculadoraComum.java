8ccd2f5e98bda46d5cd9fa086083ffe431d55181
Izabella B Lopes <izabella.bl@hotmail.com>
2022-05-09 21:01:37 -0300

    Criação da classe CalculadoraComum

--- AtividadePraticaN2/src/CalculadoraComum.java
+++ AtividadePraticaN2/src/CalculadoraComum.java
@@ -1,34 +1,34 @@
 import java.util.Scanner;
-public class CalculadoraComum extends Calculadora { 
-Scanner in = new Scanner (System.in);
-	
- public double soma ()  { 
-	 return getNum1()+getNum2();
-	}
-	
- public double subtracao () {
-	 return getNum1()-getNum2();
- }
- public double multiplicacao() {
-	 return getNum1()*getNum2(); 
- }
-public double dividir() {
-	
-	double Num2 = 0;
-if (getNum2() == 0) {
-while (Num2 ==0) {
-	System.out.printf("Digite um número diferente de zero");
-	Num2 = in.nextDouble();
-}
-setNum2(Num2);
+public class CalculadoraComum extends Calculadora {
+	Scanner in = new Scanner (System.in);
 
-}
-	
-return getNum1()/getNum2();
-
-		
+	public double soma ()  {
+		return getNum1()+getNum2();
 	}
-	
-	
-	
+
+	public double subtracao () {
+		return getNum1()-getNum2();
+	}
+	public double multiplicacao() {
+		return getNum1()*getNum2();
+	}
+	public double dividir() {
+
+		double Num2 = 0;
+		if (getNum2() == 0) {
+			while (Num2 ==0) {
+				System.out.printf("Digite um numero diferente de zero");
+				Num2 = in.nextDouble();
+			}
+			setNum2(Num2);
+
+		}
+
+		return getNum1()/getNum2();
+
+
+	}
+
+
+
 }
